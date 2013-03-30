ef5b8e701ec5188a89b6210144a2925b7a6fb6d5
epimobile <epimobile@appllab.local>
2013-03-30 13:14:44 +0100

    Mise en place d'un login screen dans le storyboard

---
+++ Milkyway/Milkyway/MKLoginScreen.h
@@ -0,0 +1,13 @@
+//
+//  MKLoginScreen.h
+//  Milkyway
+//
+//  Created by epimobile on 30/03/13.
+//  Copyright (c) 2013 epimobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKLoginScreen : UIViewController
+
+@end
---
+++ Milkyway/Milkyway/MKLoginScreen.m
@@ -0,0 +1,38 @@
+//
+//  MKLoginScreen.m
+//  Milkyway
+//
+//  Created by epimobile on 30/03/13.
+//  Copyright (c) 2013 epimobile. All rights reserved.
+//
+
+#import "MKLoginScreen.h"
+
+@interface MKLoginScreen ()
+
+@end
+
+@implementation MKLoginScreen
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
